f21fa581d6314438609194a42ee49b5279f3091a
orhun-kolgeli <okolgel1@swarthmore.edu>
2021-08-16 00:30:22 +0300

    display the encyclopedia entry given in the URL

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,5 @@
 from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse 
 from markdown2 import Markdown
 
 from . import util
@@ -12,10 +11,18 @@ def index(request):
     })
 
 def titles(request, title):
-    mdfile = util.get_entry(title)
-    markdowner = Markdown()
-    html = markdowner.convert(mdfile)
-    return render(request, "encyclopedia/entry.html", {
-        "entry": html
-    })
+    if title in util.list_entries():
+        mdfile = util.get_entry(title)
+        markdowner = Markdown()
+        html = markdowner.convert(mdfile)
+        return render(request, "encyclopedia/entry.html", {
+            "entry": html,
+            "title": title
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry": "",
+            "title": "Not found"
+        })
+    
 
