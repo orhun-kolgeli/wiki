dec509475f22e746c18759a4f0ad050ea714e8aa
orhun-kolgeli <okolgel1@swarthmore.edu>
2021-08-15 23:20:14 +0300

    implement entry page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import HttpResponse 
+from markdown2 import Markdown
 
 from . import util
 
@@ -6,5 +9,13 @@ from . import util
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def titles(request, title):
+    mdfile = util.get_entry(title)
+    markdowner = Markdown()
+    html = markdowner.convert(mdfile)
+    return render(request, "encyclopedia/entry.html", {
+        "entry": html
     })
 
