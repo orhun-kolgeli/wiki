cdbf780b578d7e44cfd0ef563a6cf2b0eee2e984
orhun-kolgeli <okolgel1@swarthmore.edu>
2021-08-20 17:23:12 +0300

    add style to "create new entry" page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,8 +9,10 @@ from . import util
 import encyclopedia
 
 class NewEntry(forms.Form):
-    title = forms.CharField(label="Title")
-    content = forms.CharField(label="Content", widget=forms.Textarea)
+    title = forms.CharField(label="Title", widget=forms.TextInput(
+        attrs={"class":"form-control"}))
+    content = forms.CharField(label="Content", widget=forms.Textarea(
+        attrs={"class":"form-control"}))
 
 class EditEntry(forms.Form):
     content = forms.CharField(label="Edit Content", widget=forms.Textarea)
